21800abb0641f79edcc90e3625156e2c76fd796c
SoloStudio <147620785+S0L0STUDI0@users.noreply.github.com>
2023-12-24 20:28:31 +0700

    Update terminal_mode_calcutor.py
    Added reset function

--- terminal_mode_calcutor.py
+++ terminal_mode_calcutor.py
@@ -6,39 +6,47 @@
 #  |_||_| |_____/ \___/|_|\___/_____/ \__|\__,_|\__,_|_|\___/     
                                    
 print('===================================================================================')
-print('! Notice This calculator does NOT support decimals and fractions in the equation!')
+print('<! Notice This calculator does NOT support decimals and fractions in the equation!>')
 print('===================================================================================')
 
-print('Choose an operation to calculate.')
-print('a. ADD')
-print('s. SUBTRACT')
-print('m. MULTIPLY')
-print('d. DIVIDE')
+def calculator():
+    print('Choose an operation to calculate.')
+    print('a. ADD')
+    print('s. SUBTRACT')
+    print('m. MULTIPLY')
+    print('d. DIVIDE')
 
-calculate = input('Enter your operation: ')
+    calculate = input('Enter your operation: ')
 
-if calculate == 'a':
-    x = int(input('Enter first number: '))
-    y = int(input('Enter second number: '))
-    print('The answer is', x + y)
-elif calculate == 's':
-    x = int(input('Enter first number: '))
-    y = int(input('Enter second number: '))
-    print('The answer is', x - y)
-elif calculate == 'm':
-    x = int(input('Enter first number: '))
-    y = int(input('Enter second number: '))
-    print('The answer is', x * y)
-elif calculate == 'd':
-    x = int(input('Enter first number: '))
-    y = int(input('Enter second number: '))
-    if y == 0:
-        print('Error: Cannot divide by zero.')
+    if calculate == 'a':
+        x = int(input('Enter first number: '))
+        y = int(input('Enter second number: '))
+        print('The answer is', x + y)
+    elif calculate == 's':
+        x = int(input('Enter first number: '))
+        y = int(input('Enter second number: '))
+        print('The answer is', x - y)
+    elif calculate == 'm':
+        x = int(input('Enter first number: '))
+        y = int(input('Enter second number: '))
+        print('The answer is', x * y)
+    elif calculate == 'd':
+        x = int(input('Enter first number: '))
+        y = int(input('Enter second number: '))
+        if y == 0:
+            print('Error: Cannot divide by zero.')
+        else:
+            print('The answer is', x / y)
     else:
-        print('The answer is', x / y)
-else:
-    print('Error: Invalid selection.')
+        print('Error: Invalid selection.')
 
+    restart = input('Do you want to perform another calculation? (yes/no): ')
+    if restart.lower() == 'yes':
+        calculator()
+    else:
+        print('Calculator has been closed.')
+
+calculator()
 
 #Developed in 2022 - Solo studio
 #Copyright © 2023 - Solo studio
